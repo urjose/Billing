8ae2a438992b57980f16f9d0e40c58d95e4937a5
Jose Manuel <urjose@yahoo.com>
2020-11-17 06:05:12 +0000

    Slice 04: Se puede capturar:  Precio Unitario

--- Billing.rb
+++ Billing.rb
@@ -1,12 +1,13 @@
 class Billing
-    def initialize(quantity)
-        @quantity = quantity
+    def initialize(quantity, uprice)
+        @quantity   = quantity
+        @uprice     = uprice
     end
 
     def mostrar
-        return "Cantidad= " + @quantity
+        return  "Cantidad= " + @quantity 
     end
 end
 
-obj_billing = Billing.new(ARGV[0])
+obj_billing = Billing.new(ARGV[0], ARGV[1])
 puts obj_billing.mostrar()
