80aab75f6e44157be8dec81f58f70f466036623c
Jose Manuel <urjose@yahoo.com>
2020-11-17 05:48:06 +0000

    Slice 02: Se agrega texto     'Cantidad= '

--- Billing.rb
+++ Billing.rb
@@ -2,6 +2,11 @@ class Billing
     def initialize(quantity)
         @quantity = quantity
     end
+
+    def mostrar
+        return "Cantidad= "
+    end
 end
 
 obj_billing = Billing.new(ARGV[0])
+puts obj_billing.mostrar()
