7fc72438291b73d1079fba6055dacbf47e6f49a4
Jose Manuel <urjose@yahoo.com>
2020-11-17 07:23:37 +0000

    Slice 14: Se busca descuento por monto. Se agrega texto:    'Descuento= '+ Descuento

--- Billing.rb
+++ Billing.rb
@@ -5,8 +5,8 @@ class Billing
         @state      = state.to_s
     end
 
-    def mostrar
-        calcular()
+    def show
+        calculate()
         output = "Cantidad= " + @quantity.to_s 
         output += "\n" 
         output += "Precio Unitario= " + @uprice.to_s 
@@ -15,15 +15,35 @@ class Billing
         output += "\n" 
         output += "Impuesto(#{@state})= "
         output += "\n"
-        output += "Descuento(#{@state})= "
+        output += "Descuento= " + @discount.to_s
         output += "\n"
         return output
     end
 
-    def calcular
+    def calculate
         @subtotal = @quantity * @uprice
+        @discount = searchDiscount()
+    end
+
+    def searchDiscount()
+        discount = {"1000" => 3, "5000" => 5, "7000" => 7, "10000" => 10, "50000" => 15}
+
+        case @subtotal
+            when 0 .. 1000
+                return 0.
+            when 1001 .. 5000
+                return discount["1000"]
+            when 5001 .. 7000
+                return discount["5000"]
+            when 7001 .. 10000
+                return discount["7000"]
+            when 10001 .. 50000
+                return discount["10000"]
+            else
+                return discount["50000"]
+            end
     end
 end
 
 obj_billing = Billing.new(ARGV[0], ARGV[1], ARGV[2])
-puts obj_billing.mostrar()
+puts obj_billing.show()
